215f0666ec1ee96780be4a3541c0a0fa11a3888b
burtsevyg <burtsevyg@mail.ru>
2019-02-21 13:29:33 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
-echo "===================================================" >> /var/log/cron.log 2>&1
-echo "$(date): executed script" >> /var/log/cron.log 2>&1
-docker system prune --all -f >> /var/log/cron.log 2>&1
-echo "" >> /var/log/cron.log 2>&1
-echo "" >> /var/log/cron.log 2>&1
-echo "" >> /var/log/cron.log 2>&1
+echo "==================================================="
+echo "$(date): executed script"
+docker system prune --all -f
+echo ""
+echo ""
+echo ""
